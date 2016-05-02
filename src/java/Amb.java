89102e7688ae346c01173879f6d2d9d961614345
Vinicius Carvalho <vinixnan@gmail.com>
2016-05-02 11:45:16 -0300

    Adding JASON version

---
+++ src/java/Amb.java
@@ -0,0 +1,17 @@
+// CArtAgO artifact code for project smadasUFSC
+
+import cartago.*;
+
+public class Amb extends Artifact {
+	void init(int initialValue) {
+		defineObsProperty("count", initialValue);
+	}
+	
+	@OPERATION
+	void inc() {
+		ObsProperty prop = getObsProperty("count");
+		prop.updateValue(prop.intValue()+1);
+		signal("tick");
+	}
+}
+
