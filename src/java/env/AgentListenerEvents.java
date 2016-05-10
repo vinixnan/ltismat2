8dff0521c60b2218251ed31eed2bbbde994758fa
Vinicius Carvalho <viniciusrenan.carvalho@gmail.com>
2016-05-10 08:57:15 -0300

    Leno Project as default

--- src/java/env/AgentListenerEvents.java
+++
@@ -1,8 +0,0 @@
-package env;
-
-import jason.asSyntax.Literal;
-
-public interface AgentListenerEvents { 
-    public void notifyPercepts(Literal... literals);
-    public void addBelief(Literal belief);
-}
--- src/java/graphLib/BestCoverageInterface.java
+++
@@ -1,8 +0,0 @@
-package graphLib;
-
-import java.util.ArrayList;
-
-public interface BestCoverageInterface {
-    public ArrayList<Integer> execute(Graph g, int paramInt);
-    public ArrayList<Integer> execute(Graph g, int paramInt, int vertexIgnore);
-}
