284d56858c25fd959575962ea0431f88c4d8afba
leno <f.leno@hotmail.com.br>
2016-05-13 12:02:38 -0300

    -a

---
+++ massimJacamoProject_bac/src/env/massimJacamoProject/ProjectConstants.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package massimJacamoProject;
+
+/**
+ * Project Constants
+ * @author Leno
+ *
+ */
+public class ProjectConstants {
+
+	//Default javaAgentConfig XML file path
+	public static final String INTERPRETER_FILE = "javaagentsconfig.xml";
+
+}
